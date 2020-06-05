038541fe59c89f2b6d6b5be6dc7e98a16cc6cdb3
somsakra <somsakra@live.com>
2020-06-05 14:36:21 +0700

    add dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -22,8 +23,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = 'ThisIsTheSecrte';
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
