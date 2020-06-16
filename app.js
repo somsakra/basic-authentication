f3b3457e17c1b68f5174ca797b5080cce5332bde
somsakra <somsakra@live.com>
2020-06-16 16:34:13 +0700

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen('3000', () => console.log('Server is running on port 3000'));
+app.listen('3000', () => console.log('running on port 3000'));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {
   useNewUrlParser: true,
